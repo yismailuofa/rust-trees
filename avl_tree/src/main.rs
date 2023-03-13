aebbe4ad9583a922153322b4e75cd7fb1fc6c674
Youssef Ismail <yismail@ualberta.ca>
2023-03-13 13:42:39 -0600

    Refactor main

--- avl_tree/src/main.rs
+++ avl_tree/src/main.rs
@@ -1,5 +1,5 @@
 use avl_tree::*;
-use tree::TreeTrait;
+use tree::prompt_user;
 
 /**
 Your library must
@@ -14,61 +14,9 @@ allow the user to perform the following operation:
 */
 
 fn main() {
-    let options = vec![
-        "Insert a node",
-        "Delete a node",
-        "Count the number of leaves",
-        "Height of the tree",
-        "Print In-order traversal of the tree",
-        "Check if the tree is empty",
-        "Print the tree showing its structure",
-    ];
-
     let mut tree = AVLTree(None);
 
     loop {
-        for (i, option) in options.iter().enumerate() {
-            println!("{}: {}", i + 1, option);
-        }
-
-        let mut input = String::new();
-        std::io::stdin().read_line(&mut input).unwrap();
-
-        match input.trim() {
-            "1" => {
-                println!("Enter the value of the node");
-                let mut input = String::new();
-                std::io::stdin().read_line(&mut input).unwrap();
-                let value = input.trim().parse::<u32>().unwrap();
-                tree.insert_node(value);
-                println!("Value inserted successfully");
-            }
-            "2" => {
-                println!("Enter the value of the node");
-                let mut input = String::new();
-                std::io::stdin().read_line(&mut input).unwrap();
-                let value = input.trim().parse::<u32>().unwrap();
-                tree.delete_node(value);
-                println!("Value deleted successfully");
-            }
-            "3" => {
-                println!("Number of leaves: {}", tree.count_leaves());
-            }
-            "4" => {
-                println!("Height of the tree: {}", tree.height());
-            }
-            "5" => {
-                println!("In-order traversal of the tree: {:?}", tree.in_order());
-            }
-            "6" => {
-                println!("Is the tree empty: {}", tree.is_empty());
-            }
-            "7" => {
-                tree.print_tree();
-            }
-            _ => {
-                println!("Invalid option");
-            }
-        }
+        prompt_user(&mut tree);
     }
 }
--- red_black_tree/src/main.rs
+++ red_black_tree/src/main.rs
@@ -1,5 +1,5 @@
 use red_black_tree::RedBlackTree;
-use tree::TreeTrait;
+use tree::prompt_user;
 
 /**
 Your library
@@ -15,61 +15,9 @@ sufficient)
 */
 
 fn main() {
-    let options = vec![
-        "Insert a node",
-        "Delete a node",
-        "Count the number of leaves",
-        "Height of the tree",
-        "Print In-order traversal of the tree",
-        "Check if the tree is empty",
-        "Print the tree showing its colors and structure",
-    ];
-
     let mut tree = RedBlackTree(None);
 
     loop {
-        for (i, option) in options.iter().enumerate() {
-            println!("{}: {}", i + 1, option);
-        }
-
-        let mut input = String::new();
-        std::io::stdin().read_line(&mut input).unwrap();
-
-        match input.trim() {
-            "1" => {
-                println!("Enter the value of the node");
-                let mut input = String::new();
-                std::io::stdin().read_line(&mut input).unwrap();
-                let value = input.trim().parse::<u32>().unwrap();
-                tree.insert_node(value);
-                println!("Value inserted successfully");
-            }
-            "2" => {
-                println!("Enter the value of the node");
-                let mut input = String::new();
-                std::io::stdin().read_line(&mut input).unwrap();
-                let value = input.trim().parse::<u32>().unwrap();
-                tree.delete_node(value);
-                println!("Value deleted successfully");
-            }
-            "3" => {
-                println!("Number of leaves: {}", tree.count_leaves());
-            }
-            "4" => {
-                println!("Height of the tree: {}", tree.height());
-            }
-            "5" => {
-                println!("In-order traversal of the tree: {:?}", tree.in_order());
-            }
-            "6" => {
-                println!("Is the tree empty: {}", tree.is_empty());
-            }
-            "7" => {
-                tree.print_tree();
-            }
-            _ => {
-                println!("Invalid option");
-            }
-        }
+        prompt_user(&mut tree);
     }
 }
