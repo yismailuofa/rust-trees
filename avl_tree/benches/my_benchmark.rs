bec30bcbac7c38914a711362ac2f445bac22e1e7
Youssef Ismail <yismail@ualberta.ca>
2023-03-16 15:04:29 -0600

    remove red squiggles 🥵

--- avl_tree/benches/my_benchmark.rs
+++ avl_tree/benches/my_benchmark.rs
@@ -1,21 +1,21 @@
-use criterion::{BenchmarkId, black_box, criterion_group, criterion_main, Criterion};
 use avl_tree::*;
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
+use tree::TreeTrait;
 
 fn test_tree(tree_size: i32) {
-    
     let mut tree = AVLTree(None);
 
     for i in 0..tree_size {
-        tree.insert_node(None, i);
+        tree.insert_node(i as u32);
     }
 
-    for i in 0..(tree_size / 10) {
-        tree.search_node(i);
+    for _ in 0..(tree_size / 10) {
+        // tree.search_node(i);
+        todo!()
     }
 }
 
 fn from_elem(c: &mut Criterion) {
-
     let mut group = c.benchmark_group("AVL_tree");
 
     for size in [10000, 40000, 70000, 100000, 130000].iter() {
--- red_black_tree/benches/my_benchmark.rs
+++ red_black_tree/benches/my_benchmark.rs
@@ -1,21 +1,21 @@
-use criterion::{BenchmarkId, black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 use red_black_tree::*;
+use tree::TreeTrait;
 
 fn test_tree(tree_size: i32) {
-    
-    let mut tree = RedBlackTree(None);
+    let mut tree = RBNode::new();
 
     for i in 0..tree_size {
-        tree.insert_node(None, i);
+        tree.insert_node(i as u32);
     }
 
-    for i in 0..(tree_size / 10) {
-        tree.search_node(i);
+    for _ in 0..(tree_size / 10) {
+        // tree.search_node(i);
+        todo!()
     }
 }
 
 fn from_elem(c: &mut Criterion) {
-
     let mut group = c.benchmark_group("RedBlack_Tree");
 
     for size in [10000, 40000, 70000, 100000, 130000].iter() {
