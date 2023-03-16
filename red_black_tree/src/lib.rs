148dd6983f2c48d32bcb369e92cc6062e8363f70
Youssef Ismail <yismail@ualberta.ca>
2023-03-16 15:01:04 -0600

    Refactor into ops 🤐

--- red_black_tree/src/lib.rs
+++ red_black_tree/src/lib.rs
@@ -1,11 +1,11 @@
-pub mod print;
+mod print;
+mod tree_ops_trait;
+mod tree_trait;
 
 use std::{
     cell::RefCell,
     rc::{Rc, Weak},
 };
-
-use tree::TreeTrait;
 
 #[derive(PartialEq, Clone, Debug)]
 pub enum Color {
@@ -26,86 +26,4 @@ pub enum RBNode {
         parent: WeakTree,
     },
     Empty,
-}
-
-impl TreeTrait for RBNode {
-    fn insert_node(&mut self, _key: u32) {
-        if let RBNode::Empty = self {
-            *self = RBNode::Node {
-                key: _key,
-                color: Color::Black,
-                left: Rc::new(RefCell::new(RBNode::Empty)),
-                right: Rc::new(RefCell::new(RBNode::Empty)),
-                parent: Weak::new(),
-            };
-            return;
-        }
-
-        let mut curr = self.clone();
-
-        while let RBNode::Node {
-            key, left, right, ..
-        } = &curr.clone()
-        {
-            if _key < *key {
-                let mut left_node = left.borrow_mut();
-
-                if let RBNode::Empty = left_node.clone() {
-                    *left_node = RBNode::Node {
-                        key: _key,
-                        color: Color::Red,
-                        left: Rc::new(RefCell::new(RBNode::Empty)),
-                        right: Rc::new(RefCell::new(RBNode::Empty)),
-                        parent: Rc::downgrade(&Rc::new(RefCell::new(curr.clone()))),
-                    };
-                    return;
-                } else {
-                    curr = left_node.clone();
-                }
-            } else if _key > *key {
-                let mut right_node = right.borrow_mut();
-
-                if let RBNode::Empty = right_node.clone() {
-                    *right_node = RBNode::Node {
-                        key: _key,
-                        color: Color::Red,
-                        left: Rc::new(RefCell::new(RBNode::Empty)),
-                        right: Rc::new(RefCell::new(RBNode::Empty)),
-                        parent: Rc::downgrade(&Rc::new(RefCell::new(curr.clone()))),
-                    };
-                    return;
-                } else {
-                    curr = right_node.clone();
-                }
-            }
-        }
-    }
-
-    fn delete_node(&mut self, _key: u32) {
-        todo!()
-    }
-
-    fn count_leaves(&self) -> u32 {
-        todo!()
-    }
-
-    fn height(&self) -> u32 {
-        todo!()
-    }
-
-    fn in_order(&self) -> Vec<u32> {
-        todo!()
-    }
-
-    fn is_empty(&self) -> bool {
-        todo!()
-    }
-
-    fn print_tree(&self) {
-        ptree::print_tree(self).expect("Failed to print tree");
-    }
-
-    fn new() -> Self {
-        RBNode::Empty
-    }
 }
