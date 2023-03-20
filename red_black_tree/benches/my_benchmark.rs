19b5dd280074b3a9dff8f6210d5708e233b9d4c7
rbapuji1 <rupin@ualberta.ca>
2023-03-19 19:40:31 -0600

    benchmark update

--- red_black_tree/benches/my_benchmark.rs
+++ red_black_tree/benches/my_benchmark.rs
@@ -1,26 +1,28 @@
-use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use red_black_tree::*;
 use tree::TreeTrait;
-
-fn test_tree(tree_size: i32) {
-    let mut tree = RBTree::default();
-
-    for i in 0..tree_size {
-        tree.insert_node(i as u32);
-    }
-
-    for _ in 0..(tree_size / 10) {
-        // tree.search_node(i);
-        todo!()
-    }
-}
 
 fn from_elem(c: &mut Criterion) {
     let mut group = c.benchmark_group("RedBlack_Tree");
 
-    for size in [10000, 40000, 70000, 100000, 130000].iter() {
-        group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
-            b.iter(|| test_tree(black_box(size)));
+    for size in [100, 400, 700, 1000, 1300].iter() {
+    // for size in [10000, 40000, 70000, 100000, 130000].iter() {
+
+        group.bench_with_input(
+            BenchmarkId::new("RedBlack_Tree", size), size, |b, &size| {
+            b.iter(|| {
+                let mut tree = RBTree::default();
+
+                // insert
+                for i in 0..size {
+                    tree.insert_node(i as u32);
+                }
+                
+                // search
+                for j in 0..(size / 10) {
+                    let x = tree.find_node(j as u32);
+                }
+            });
         });
     }
     group.finish();
