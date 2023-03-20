71f5985642525d7145746462c51aea3dc23d56e4
Youssef Ismail <yismail@ualberta.ca>
2023-03-20 11:11:05 -0600

    ropin bro

--- avl_tree/benches/my_benchmark.rs
+++ avl_tree/benches/my_benchmark.rs
@@ -3,10 +3,9 @@ use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use tree::TreeTrait;
 
 fn from_elem(c: &mut Criterion) {
-    let mut group = x.benchmark_group("AVL_Tree");
+    let mut group = c.benchmark_group("AVL_Tree");
 
-        for size in [10000, 40000, 70000, 100000, 130000].iter() {
-
+    for size in [10000, 40000, 70000, 100000, 130000].iter() {
         group.bench_with_input(BenchmarkId::new("AVL_Tree", size), size, |b, &size| {
             b.iter_with_large_drop(|| {
                 let mut tree = AVLTree::default();
@@ -15,7 +14,7 @@ fn from_elem(c: &mut Criterion) {
                 for i in 0..size {
                     tree.insert_node(i as u32);
                 }
-                
+
                 // search
                 for j in 0..(size / 10) {
                     find_node(&tree.root, j as u32);
